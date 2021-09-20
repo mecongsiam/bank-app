3206c9660480707151aef7f6d8ce5c3d322a3955
mecong <mecongsiam@gmail.com>
2021-09-20 23:41:58 +0200

    init

---
+++ user-management/user.cmd.api/src/main/java/com/springbank/user/cmd/api/UserCommandApplication.java
@@ -0,0 +1,21 @@
+package com.springbank.user.cmd.api;
+
+import com.springbank.user.core.configuration.AxonConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+
+//@EnableResourceServer
+//@EnableGlobalMethodSecurity(prePostEnabled = false)
+@SpringBootApplication
+@Import({ AxonConfig.class })
+public class UserCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCommandApplication.class, args);
+	}
+
+}
